520e34d35ef83e30e019d55f47d3aeb55dfca620
Rafli <raflimramadhan.204@gmail.com>
2026-06-22 14:58:46 +0700

    new

--- config/cors.go
+++ config/cors.go
@@ -3,7 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"http://localhost:5174",
-	"belatihan-production-33a2.up.railway.app",
+	"https://belatihan-production-33a2.up.railway.app",
 }
 
 func GetAllowedOrigins() []string {
